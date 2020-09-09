bd3291a92f61e973a12460c586944377de851d71
Rene Koerner <rene.koerner@volkswagen.de>
2020-09-09 19:26:22 +0200

    changed import

--- VerordnungenImportService/src/main/java/com/vw/fakultaet73/VerordnungenImportService/VerordnungenImportService/Controller/ReceiveData.java
+++ VerordnungenImportService/src/main/java/com/vw/fakultaet73/VerordnungenImportService/VerordnungenImportService/Controller/ReceiveData.java
@@ -1,27 +1,16 @@
 package com.vw.fakultaet73.VerordnungenImportService.VerordnungenImportService.Controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.vw.fakultaet73.VerordnungenImportService.VerordnungenImportService.entitites.DecreeEntity;
 import com.vw.fakultaet73.VerordnungenImportService.VerordnungenImportService.services.ImportService;
@@ -44,13 +33,15 @@ public class ReceiveData {
 			return this.importService.saveDecrees((parse()));
 	}
 	
-	public List<DecreeEntity> parse() {
-		Client client = ClientBuilder.newClient();
-		Response response = client.target(GET_URL)
-			.request(MediaType.APPLICATION_JSON)
-			.get();
-		return response.readEntity(new GenericType<List<DecreeEntity>>() {});
-
-		
+	private List<DecreeEntity> parse() {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<DecreeEntity[]> response = restTemplate.getForEntity(this.GET_URL,DecreeEntity[].class);
+		DecreeEntity[] decrees = response.getBody();
+		List<DecreeEntity> decreesList = new ArrayList<>();
+		for (DecreeEntity decreeEntity : decrees) {
+			decreesList.add(decreeEntity);
+		}
+		return decreesList;
 	}
+	
 }
